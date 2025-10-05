7397cb1dc3a8b88457c12e43c263ca1e35b971ca
Ashirwad Shetye <ashirwad.taiyo@gmail.com>
2025-10-05 16:23:38 +0530

    chores

--- src/constants/nav-items.js
+++ src/constants/nav-items.js
@@ -3,9 +3,9 @@ import { Icons } from "@/components/ui/icons";
 export const NAV_ITEMS = [
 	{ icon: Icons.Home, label: "Home", id: "home" },
 	{ icon: Icons.Briefcase, label: "Portfolios", id: "portfolios" },
-	{ icon: Icons.Beaker, label: "Experimentals", id: "experimentals" },
-	{ icon: Icons.Archive, label: "Slack Archives", id: "slack" },
-	{ icon: Icons.Refer, label: "Refer a friend", id: "refer" },
-	{ icon: Icons.Gift, label: "Gift a subscription", id: "gift" },
-	{ icon: Icons.User, label: "Account", id: "account" },
+	{ icon: Icons.Beaker, label: "Experimentals", id: "" },
+	{ icon: Icons.Archive, label: "Slack Archives", id: "" },
+	{ icon: Icons.Refer, label: "Refer a friend", id: "" },
+	{ icon: Icons.Gift, label: "Gift a subscription", id: "" },
+	{ icon: Icons.User, label: "Account", id: "" },
 ];
